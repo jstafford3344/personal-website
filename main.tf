db8dbddc9a65d26b789a3ed11313c651572aabc4
jstafford3344 <jstafford@getsporttrade.com>
2024-09-01 15:19:36 -0400

    chore: testing

--- main.tf
+++ main.tf
@@ -62,4 +62,6 @@ resource "null_resource" "ansible_provision" {
       ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i '${digitalocean_floating_ip.web_server_static_ip.ip_address},' -u root --private-key ~/.ssh/id_rsa ansible/site.yml
     EOT
   }
+
+  depends_on = [digitalocean_floating_ip_assignment.web_server_static_ip_assignment]
 }
